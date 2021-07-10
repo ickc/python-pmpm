958225cf63061261c5bc02cb730db1874511e1f4
Kolen Cheung <christian.kolen@gmail.com>
2021-07-09 19:42:24 -0700

    default to log INFO

--- src/pmpm/__init__.py
+++ src/pmpm/__init__.py
@@ -13,8 +13,8 @@ handler = logging.StreamHandler()
 logger.addHandler(handler)
 handler.setFormatter(Formatter('%(name)s %(levelname)s %(message)s'))
 try:
-    level = os.environ.get('PMPMLOGLEVEL', logging.WARNING)
+    level = os.environ.get('PMPMLOGLEVEL', logging.INFO)
     logger.setLevel(level=level)
 except ValueError:
-    logger.setLevel(level=logging.WARNING)
-    logger.error(f'Unknown PMPMLOGLEVEL {level}, set to default WARNING.')
+    logger.setLevel(level=logging.INFO)
+    logger.error(f'Unknown PMPMLOGLEVEL {level}, set to default INFO.')
