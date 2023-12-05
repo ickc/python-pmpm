a1d6f746085ac0de808693b75850fb16633101a1
Kolen Cheung <christian.kolen@gmail.com>
2023-12-05 21:15:22 +0000

    fix mamba discovery on windows

--- src/pmpm/core.py
+++ src/pmpm/core.py
@@ -285,7 +285,7 @@ class InstallEnvironment(metaclass=DocInheritMeta(style="google_with_merge")):
     @cached_property
     def mamba_bin(self) -> Path:
         """Path to the mamba binary."""
-        path = self.conda_root_prefix / "Scripts" / "mamba.exe" if self.is_windows else self.conda_root_prefix / "bin" / "mamba"
+        path = self.conda_root_prefix / "Scripts" / "mamba" if self.is_windows else self.conda_root_prefix / "bin" / "mamba"
         try:
             check_file(path, "binary located at %s")
             return path
