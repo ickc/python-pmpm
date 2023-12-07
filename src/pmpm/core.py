7980e4bd11bb47b0d040caef3c6802ec8515e860
Kolen Cheung <christian.kolen@gmail.com>
2023-12-07 03:33:11 +0000

    revert 29886ef

--- src/pmpm/core.py
+++ src/pmpm/core.py
@@ -365,26 +365,12 @@ class CondaOnlyEnvironment(InstallEnvironment):
     conda_prefix_name: str = ""
     compile_prefix_name: str = ""
     environment_variable: ClassVar[Tuple[str, ...]] = (
-        "_CE_CONDA",  # conda
-        "_CE_M",  # conda
-        "CONDA_DEFAULT_ENV",  # conda
         "CONDA_EXE",  # conda
         "CONDA_PREFIX",  # conda
-        "CONDA_PROMPT_MODIFIER",  # conda
-        "CONDA_PYTHON_EXE",  # conda
-        "CONDA_SHLVL",  # conda
-        "CPL_ZIP_ENCODING",  # conda
-        "GDAL_DATA",  # conda
-        "GDAL_DRIVER_PATH",  # conda
-        "GSETTINGS_SCHEMA_DIR_CONDA_BACKUP",  # conda
-        "GSETTINGS_SCHEMA_DIR",  # conda
         "HOME",  # UNIX
-        "PROJ_DATA",  # conda
-        "PROJ_NETWORK",  # conda
         "SYSTEMROOT",  # Windows, usually points to C:\Windows
         "TERM",  # UNIX
         "USERPROFILE",  # Windows, usually points to C:\Users\USERNAME
-        "XML_CATALOG_FILES",  # conda
     )
     sanitized_path: ClassVar[Tuple[str, ...]] = ("/bin", "/usr/bin")  # needed for conda to find POSIX executables
 
